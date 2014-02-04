d61b5668695a9596797ddd958cb8ecd74e930db6
Tim Macdonald <tsmacdonald@gmail.com>
2014-02-03 20:34:11 -0600

    Add basic cat display functionality

--- cat.py
+++ cat.py
@@ -1,4 +1,4 @@
-class Cat():
+class Util():
   def __init__(self, files):
     self.files = files
   def run(self):
@@ -6,3 +6,18 @@ class Cat():
       with open(file) as f:
         for line in f:
           print line,
+def show():
+  print """
+ /\     /\\
+{  `---'  }
+{  O   O  }
+~~>  V  <~~
+ \  \|/  /
+  `-----'__
+  /     \  `^\_
+ {       }\ |\_\_   W
+ |  \_/  |/ /  \_\_( )
+  \__/  /(_E     \__/
+    (  /
+     MM
+     """
